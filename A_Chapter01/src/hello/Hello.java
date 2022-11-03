51ec8d76cec25710c713d6011b8cdaee5e7c1c71
DESKTOP-MIJ53UH\user2 <kwon524@naver.com>
2022-11-03 21:28:20 +0900

    자바 프로그래밍 시작하기

---
+++ A_Chapter01/src/hello/Hello.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Hello {
+
+	/*
+	 * 주석
+	 *	- 소스 코드와 무관하고 코드로 인식되지 않는다.
+	 *	- 주로 코드에 대한 설명을 작성할 때 사용한다.
+	 */
+
+	// 한 줄 주석
+//	ctrl+/ 추가로 주석 작성
+	
+	public static void main(String[] args) {
+		// 실행될 문장(코드)들을 적는다.
+		System.out.println("Hello");
+		System.out.println("안녕하세요!");
+		System.out.println("Hello, Java");
+	}
+
+}
